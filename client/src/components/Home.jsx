08eb7f25545e9261e0ad6f17e72f75c97ee1eb0b
Oswaldo Palacios Perez <yaco2002@live.com.mx>
2025-01-16 22:32:54 -0600

    updating

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./Navbar";
 import logitech from "../assets/logitech.png";
+import banner from '../assets/promo.jpg'
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen">
       <Navbar />
 
-      <header className="bg-[#32176D] w-full h-[530px]">
-        <nav className="flex px-40 pt-6 justify-between">
+      <header className="bg-[#32176D] w-full h-[530px] relative">
+        <nav className="flex px-40 pt-6 justify-between z-[100] relative">
           <div>
             <img className="w-32" src={logitech} alt="" />
           </div>
@@ -21,10 +22,18 @@ export default function Home() {
           </div>
 
           <div>
+            <div>
             <input className="bg-transparent" type="text" placeholder="Buscar" />
+            </div>
           </div>
 
         </nav>
+
+        <div>
+          <h2 className="text-3xl text-white font-semibold">NUEVO AÑO, NUEVO EQUIPO</h2>
+        </div>
+
+        <img className="absolute w-[890px] h-full top-0 right-0 z-10 object-cover object-left " style={{ clipPath: "polygon(10% 0, 100% 0, 100% 100%, 0% 100%)" }} src={banner} alt="" />
       </header>
     </div>
   );
